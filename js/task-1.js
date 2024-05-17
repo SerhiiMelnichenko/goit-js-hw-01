0b1863b7a044dc61a9ae419d7414e3a02d840629
Sergey Melnichenko <159032191+SerhiiMelnichenko@users.noreply.github.com>
2024-05-17 17:13:25 +0300

    commid

---
+++ js/task-1.js
@@ -0,0 +1,10 @@
+const totalPrice = "quantity" * "pricePerDroid"
+
+
+function makeTransaction(quantity, totalPrice) {
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`
+}
+
+console.log(makeTransaction(5, 3000)) // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000)) // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 500)) // "You ordered 10 droids worth 5000 credits!"
