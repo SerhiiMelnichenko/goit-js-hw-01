6739bc1ba585c1daebd902b7cb745aea90373998
Sergey Melnichenko <159032191+SerhiiMelnichenko@users.noreply.github.com>
2024-05-17 20:43:16 +0300

    takoe

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,11 @@
-const totalPrice = "quantity" * "pricePerDroid"
-
 
 function makeTransaction(quantity, totalPrice) {
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`
+   totalPrice = quantity * pricePerDroid
+}
+
+function makeTransaction(quantity, totalPrice) {
+    console.log (
+        return `You ordered ${quantity} droids worth ${totalPrice} credits!`);
 }
 
 console.log(makeTransaction(5, 3000)) // "You ordered 5 droids worth 15000 credits!"
